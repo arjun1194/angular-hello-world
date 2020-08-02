ba684b1b3011d369297abeb828893e7930ac30ba
arjun <arjunbhd1194@gmail.com>
2020-08-02 14:18:21 +0530

    Added Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AOLBtnComponent } from './aol-btn/aol-btn.component';
+import {FormsModule} from '@angular/forms';
+import { FormListItemComponent } from './aol-btn/form-list-item/form-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AOLBtnComponent,
+    FormListItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
