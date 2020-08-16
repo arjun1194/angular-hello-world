430eee0b495c24f5ab579ffccc504a41d8276530
Jain <yj185020@ncr.com>
2020-08-16 16:27:13 +0530

    Modal not closing properly

--- src/app/aol-btn/modal-form/modal-form.component.ts
+++ src/app/aol-btn/modal-form/modal-form.component.ts
@@ -7,6 +7,9 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class ModalFormComponent implements OnInit {
   @Input('heading') heading;
+  public valueSetFlag = false;
+  public aolFlag = false;
+  public vsvFlag = false;
 
   inputProps: {
     valueSet: [
@@ -16,11 +19,50 @@ export class ModalFormComponent implements OnInit {
       }
     ]
   };
-  modalTitle: String = "Somethin";
+  modalTitle: String = "AOL Comp";
 
   constructor() {}
 
   ngOnInit(): void {
   }
 
+  public openValueSet(field){
+    console.log("this.aolFlag",this.aolFlag)
+    if(field == 'valueset'){
+      this.valueSetFlag = true;
+      this.vsvFlag = false;
+      this.aolFlag = true;
+      
+      
+      
+    }
+    else 
+
+    console.log("this.aolFlag",this.aolFlag)
+    console.log("this.valueSetFlag",this.valueSetFlag)
+    
+
+    if(field == 'valuesetvalue'){
+      this.vsvFlag = true;
+      this.valueSetFlag = false;
+      
+    
+      
+    }
+      
+    console.log("this.aolFlag",this.aolFlag)
+    console.log("this.vsvFlag",this.vsvFlag)
+
+    
+
+  }
+  //function that closes a modal with id
+  public closeMod(id){
+    //$(id).modal('hide')
+  }
+  public closeModal(){
+    this.aolFlag = false;
+    this.valueSetFlag = false;
+    this.vsvFlag = false;
+  }
 }
